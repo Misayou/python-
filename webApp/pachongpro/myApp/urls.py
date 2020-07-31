a64e34ce7d6e8cc8f60bf483269b3246d2c7f436
h1916955160 <1916955160@qq.com>
2020-07-31 15:57:10 +0800

    数据展示，增删查改操作

--- webApp/pachongpro/myApp/urls.py
+++ webApp/pachongpro/myApp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('info', views.info)
+    path('info_show', views.info_show),
+    path('add', views.add),
+    path('delete', views.delete),
+    path('update', views.update)
 ]
